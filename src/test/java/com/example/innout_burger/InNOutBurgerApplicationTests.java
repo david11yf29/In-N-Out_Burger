6bc5ed355a8743447f05e52c1a346f45b6e505c5
David Lin <david11yf29@gmail.com>
2024-07-15 09:30:51 +0800

    second commit

--- src/test/java/com/example/innout_burger/InNOutBurgerApplicationTests.java
+++ src/test/java/com/example/innout_burger/InNOutBurgerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InNOutBurgerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
