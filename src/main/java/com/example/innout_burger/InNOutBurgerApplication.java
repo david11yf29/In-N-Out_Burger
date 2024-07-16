f4275ccaef769d96d099bd93f68503581c5faf4e
David Lin <david11yf29@gmail.com>
2024-07-16 09:45:01 +0800

    upload static resource for both backdend/ and front/

--- src/main/java/com/example/innout_burger/InNOutBurgerApplication.java
+++ src/main/java/com/example/innout_burger/InNOutBurgerApplication.java
@@ -1,13 +1,15 @@
 package com.example.innout_burger;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j // Simple Logging Facade for Java
 @SpringBootApplication
 public class InNOutBurgerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(InNOutBurgerApplication.class, args);
+        log.info("[David] Project Start Successfully...");
     }
 
 }
