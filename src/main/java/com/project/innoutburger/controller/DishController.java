63ae360d746b947d3631be5a937cf047abe46407
David Lin <david11yf29@gmail.com>
2024-07-23 17:06:03 +0800

    Finished function in order to display dish category on dish page

--- src/main/java/com/project/innoutburger/controller/DishController.java
+++ src/main/java/com/project/innoutburger/controller/DishController.java
@@ -1,15 +1,21 @@
 package com.project.innoutburger.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.project.innoutburger.common.R;
 import com.project.innoutburger.dto.DishDto;
+import com.project.innoutburger.entity.Category;
+import com.project.innoutburger.entity.Dish;
+import com.project.innoutburger.service.CategoryService;
 import com.project.innoutburger.service.DishFlavorService;
 import com.project.innoutburger.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * 菜品管理
@@ -25,6 +31,9 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     /*
     * 新增菜品
     * @param dishdto
@@ -39,4 +48,54 @@ public class DishController {
         return R.success("新增菜品成功");
     }
 
+    /*
+     * 菜品信息分頁查詢
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     * */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+
+        // 構造分頁構造器
+        Page<Dish> pageInfo = new Page<Dish>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
+
+        // 條件構造器
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加過濾條件
+        queryWrapper.like(name != null, Dish::getName, name);
+        // 添加排序條件
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        // 執行分頁查訊
+        dishService.page(pageInfo, queryWrapper);
+
+        // 對象拷貝
+        BeanUtils.copyProperties(pageInfo, dishDtoPage, "records");
+
+        List<Dish> records = pageInfo.getRecords();
+
+        List<DishDto> list = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+
+            BeanUtils.copyProperties(item, dishDto);
+
+            // 分類 id
+            Long categoryId = item.getCategoryId();
+            // 根據 id 查詢分類對象
+            Category category = categoryService.getById(categoryId);
+
+            if(category != null) {
+                String categoryName = category.getName();
+                dishDto.setCategoryName(categoryName);
+            }
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+
+        return R.success(dishDtoPage);
+    }
 }
